28ac5958356ecbad8301e7f799545aa00333bacb
McCall Woodward <woodwardem@g.crookcoountyschools.org>
2021-02-12 14:05:33 -0800

    working on functions

--- script/script.js
+++ script/script.js
@@ -1,2 +1,27 @@
-'use strict';
-console.log('Hello World');
+//'use strict';
+//console.log('Hello World');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function add(a, b) {
+    let sum = a+b;
+    return sum;
+
+}
+
+let b = 5;
+let a = 1;
+
+console.log(a+b);
