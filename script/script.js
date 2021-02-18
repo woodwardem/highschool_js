151b4a0395a32fd5cf2079235e853159b75756e0
McCall Woodward <woodwardem@g.crookcoountyschools.org>
2021-02-18 08:59:38 -0800

    learned about return values in js funtions

--- script/script.js
+++ script/script.js
@@ -19,17 +19,19 @@
 //JavaScript Loops
 //While Loop
 //If you find yourself in an infinite loop use control c
-let n = 0;
-while ( n > 3) {
-    console.log(n);
-    n++;
-}
-console.log(n);
 
-//For loop;
-let str = '';
-for (let i =0; i == 9, i++ ) {
-console.log(str + 'at' + i + ' iteration')
-str = str + i;
+
+
+
+let phrase = 'JavaScript is a super fun laguage';
+
+function printPhrase(speak) {
+
+
+    let myspeak = speak + 'and it\'s cool';
+return myspeak;
+
 }
-console.log(str);
+
+console.log(phrase);
+console.log(printPhrase(phrase));
